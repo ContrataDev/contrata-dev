083783a5ebca9b9c614e406675e2bdd798d880ff
DeyvidBorges01 <deyvid.borges01@outlook.com>
2025-11-06 22:17:21 -0300

    feat(app.js): add config passport on express framework

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import { fileURLToPath } from "url";
 import path from "path";
 import express from "express";
+import passport from "passport";
 
 import Routers from "./config/routers.js";
 
@@ -10,6 +11,8 @@ const __dirname = path.dirname(__filename);
 
 // Banco de dados
 import "./models/index.js";
+// import auth config
+import "./config/passport-config.js";
 
 // Servidor Web
 const app = express();
@@ -23,6 +26,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Usar as rotas
 app.use("/", Routers);
